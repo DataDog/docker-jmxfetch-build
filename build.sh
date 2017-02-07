ea88ac93b2b59f07b1d1d930e1f515a0a88858e3
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-02-07 15:14:38 -0500

    [builder] make jmxfetch branch customizable.

--- build.sh
+++ build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 JMXPATH=${JMXPATH:-jmxfetch}
+JMX_BRANCH=${JMX_BRANCH:-master}
 
 set -e
 
 if ! git -C $JMXPATH rev-parse 2>/dev/null;
 then
-    git clone --depth 1 https://github.com/DataDog/jmxfetch.git $JMXPATH
+    git clone --branch $JMX_BRANCH --depth 1 https://github.com/DataDog/jmxfetch.git $JMXPATH
 fi
 
-cd $JMXPATH && git pull && mvn clean compile assembly:single && cd -
+cd $JMXPATH
+if [ "$(git branch | grep -e '^\*' | cut -f2 -d' ')" != "$JMX_BRANCH" ];
+then
+    if [ -f $(git rev-parse --git-dir)/shallow ];
+    then
+        git fetch --unshallow
+    fi
+    git checkout $JMX_BRANCH
+else
+    git pull
+fi
+
+mvn clean compile assembly:single && cd -
 
 cp /$JMXPATH/target/*.jar /pkg
 
